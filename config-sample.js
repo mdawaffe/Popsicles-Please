0333318c1979762e7a1b9914f10708fe72dc22d8
Michael D Adams <michael.d.adams@gmail.com>
2014-05-16 03:00:35 -0700

    use environment variables for config

--- config-sample.js
+++ config-sample.js
@@ -3,21 +3,32 @@ module.exports = config;
 
 // Server
 config.server = {
-	port: 3000,         // port for server to listen on
-	cache: 'cache.json' // path to the cache file
-	cacheTTL: 86400000, // time for which to cache each item (ms)
+	// port for server to listen on
+	port    : process.env.PP_PORT      || 80,
+
+	// path to the cache file
+	cache   : process.env.PP_CACHE     || 'cache.json',
+
+	// time for which to cache each item (ms)
+	cacheTTL: process.env.PP_CACHE_TTL || 14400000
 }
 
 // Forecast.io
 config.ForecastIO = {
-	APIKey: process.env.FORECAST_API_KEY, // API key
-	timeout: 10000                        // HTTP request timeout (ms)
+	// API key
+	APIKey : process.env.PP_FORECAST_API_KEY,
+
+	// HTTP request timeout (ms)
+	timeout: process.env.PP_FORECAST_API_TIMEOUT || 10000
 };
 
 // Google Geocoding
 config.GoogleMaps = {
-	APIKey: process.env.GOOGLE_MAPS_API_KEY, // API key
-	timeout: 2000                            // HTTP request timeout (ms)
+	// API key
+	APIKey : process.env.PP_GOOGLE_MAPS_API_KEY,
+
+	// HTTP request timeout (ms)
+	timeout: process.env.PP_GOOGLE_MAPS_API_TIMEOUT || 2000
 };
 
 // Locations
@@ -26,9 +37,10 @@ config.GoogleMaps = {
 // Optional: URL frow which to parse locations.
 // We look for [geo microformats](http://www.microformats.org/wiki/geo)
 // (but we're not very smart at parsing them).
-//config.locationsURL = 'http://example.com/';
+config.locationsURL = process.env.PP_LOCATIONS_URL;
 
 // Optional: Array of [ lat, lng ] points.
 config.locations = [
 	[  34.147639, -118.143863 ]
+	//, ...
 ];
